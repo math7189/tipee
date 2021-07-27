ff9bc5c4828914df236a757a3717b40eb37d47f2
math <mathieu.labaune@gmail.com>
2021-07-27 20:58:12 +0200

    .

--- js/core.js
+++ js/core.js
@@ -2169,6 +2169,13 @@ class TipeeTileText extends TipeeTile {
 
 
         const requestCallback = function (returned_data) {
+            this.__UIElements.UITileContent.style.height = height + 'px';
+        this.__UIElements.UITileContent.style.position = 'absolute';
+        this.__UIElements.UITileContent.style.color = this.textColor;
+        this.__UIElements.UITileContent.style.fontFamily = this.textFont;
+        this.__UIElements.UITileContent.style.fontSize = this.textFontSize + 'px';
+        this.__UIElements.UITileContent.style.display = 'flex';
+        this.__UIElements.UITileContent.style.overflow = 'hidden';
             if(returned_data != "Error"){
             that.__UIElements.UITileContent.innerHTML = `<p style:'color: #` + that.textColor + `;'  id='`
                 + that.idTile + ` contentTxt'>` + that.textBefore + ' ' + returned_data + ' ' + that.textAfter +
